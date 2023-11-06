77a413eb197e0f2638f5a418ff4805a523653662
Alexey Stupin <gmpohta@gmail.com>
2023-11-06 16:12:15 +0800

    progress

--- src/Controller/API/CalculatorController.php
+++ src/Controller/API/CalculatorController.php
@@ -11,6 +11,7 @@ use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -51,6 +52,8 @@ final class CalculatorController extends BaseController
             }
 
             $data = $this->calculatorService->calculate($dto);
+        } catch (\JsonException|NotNormalizableValueException $e) {
+            return $this->error($e->getMessage(), 400);
         } catch (\Throwable $e) {
             return $this->error($e->getMessage());
         }
